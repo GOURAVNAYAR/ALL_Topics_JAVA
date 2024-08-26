bacc7eef97ef30df42ebcfc7a239274fcdc90990
Gourav Nayar <127810086+GOURAVNAYAR@users.noreply.github.com>
2024-08-26 13:18:13 +0530

    Update Nested_Try_Block.java

--- src/com/Exception/Nested_Try_Block.java
+++ src/com/Exception/Nested_Try_Block.java
@@ -5,7 +5,7 @@ public class Nested_Try_Block {
 		try {
 			int a = 10, b = 10, c;
 			c = a / b;
-			System.out.println(c);
+			System.out.println(c + "............");
 			try {
 				int h[] = { 10, 20, 30, 40, 50 };
 				System.out.println(h[10]);
