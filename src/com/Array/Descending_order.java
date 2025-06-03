a188575c1891be6b8dadca8c5dbf7b0978849c80
Gourav Nayar <127810086+GOURAVNAYAR@users.noreply.github.com>
2025-06-03 11:26:21 +0530

    Update Descending_order.java

--- src/com/Array/Descending_order.java
+++ src/com/Array/Descending_order.java
@@ -7,7 +7,7 @@ public class Descending_order {
 	public static void main(String[] args) {
 
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("Enter a Size of Array.......");
+		System.out.println("Enter a Size of Array.........");
 		int size = scanner.nextInt();
 		int arr[] =new int[size];
 		
